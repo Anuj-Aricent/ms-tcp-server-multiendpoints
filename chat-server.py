a0bb46dfa70cc06b2eeaa2551062c4340a638310
Anuj-Aricent <34433129+Anuj-Aricent@users.noreply.github.com>
2018-01-05 12:14:56 +0530

    Update chat-server.py
    enabling Multi sockets

--- chat-server.py
+++ chat-server.py
@@ -2,23 +2,35 @@
 import socket     
 import select         
 
-s = socket.socket()         
+s1 = socket.socket()
+s2 = socket.socket()
 host = "0.0.0.0" 
-port = 12345                
-s.bind((host, port))        
+port1 = 12345
+port2 = 12346
+s1.bind((host, port1))
+s2.bind((host, port2))
 userlist = []
 
-s.listen(5)
+s1.listen(5)
+s2.listen(5)
 CONNECTION_LIST = []
-CONNECTION_LIST.append(s)                 
+CONNECTION_LIST.append(s1)
+CONNECTION_LIST.append(s2)
 while True:
    read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
    
    for sock in read_sockets:
         #New connection
-        if sock == s:
+        if sock == s1:
             # Handle the case in which there is a new connection recieved through server_socket
-            c, addr = s.accept()
+            c, addr = s1.accept()
+            CONNECTION_LIST.append(c)
+            print "Client (%s, %s) connected" % addr
+            if c not in userlist:
+                userlist.append(c)
+        elif sock == s2:
+            # Handle the case in which there is a new connection recieved through server_socket
+            c, addr = s2.accept()
             CONNECTION_LIST.append(c)
             print "Client (%s, %s) connected" % addr
             if c not in userlist:
